6a455e7796f9e6d118374e6bb897bd8b82ad481d
alonmega100 <alon1malka@gmail.com>
2019-12-01 12:57:34 +0200

    Listening to multiple clients using threads

--- Code/client.py
+++ Code/client.py
@@ -9,25 +9,37 @@ import time
 SERVER_ADDRESS = ("127.0.0.1", 4261)
 
 
-class Client:
-    def __init__(self, _send_socket, _receive_socket):
+class Client(object):
+    def __init__(self, socket, address):
         self._message_list = []
-        self._send_socket = _send_socket
-        self._receive_socket = _receive_socket
+        self._socket = socket
+        self._address = address
 
     def connect_to_server(self):
-        self._send_socket.connect(SERVER_ADDRESS)
-        self._receive_socket.connect(SERVER_ADDRESS)
+        self._socket.connect(SERVER_ADDRESS)
 
     def send_message(self):
         text = "Phortophintesa, pizoozim"
-        self._send_socket.send(text)
+        self._socket.send(text)
+
+    def recive_message(self):
+        try:
+            data = self._socket.recv(1024)
+        except:
+            print("An error occurred while listening to server")
 
     def start(self):
         self.connect_to_server()
+        listen_to_server_thread\
+            = threading.Thread(target=self.recive_message())
+        listen_to_server_thread.start()
 
 
-s1 = socket.socket()
-s2 = socket.socket()
-a = Client(s1, s2)
-a.start()
+def main():
+    s1 = socket.socket()
+    a = Client(s1, SERVER_ADDRESS)
+    a.start()
+
+
+if __name__ == '__main__':
+    main()
