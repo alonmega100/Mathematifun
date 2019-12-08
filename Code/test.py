7a9de6101689b66797ddd66bf09225fa8c5ccc00
alonmega100 <alon1malka@gmail.com>
2019-12-08 13:02:55 +0200

    Chat works PEP8 for 2 main files.

--- Code/test.py
+++ Code/test.py
@@ -11,24 +11,20 @@ LIST_OF_MESSAGES = ["hey", "heloo", "wasap", "hi", "heyo"]
 
 
 def main():
-    i = input()
-    if i == "1":
-        s1 = socket.socket()
-        s1.connect(SERVER_ADDRESS)
-        print("conected")
+    s1 = socket.socket()
+    s1.connect(SERVER_ADDRESS)
+    print("conected")
+    c = input()
+    if c == "s":
+        s1.send(("01000" + LIST_OF_MESSAGES[random.randint(0, 4)]).encode())
+        print("sent")
+    if c == "r":
         print("reciving")
         data = s1.recv(1024)
         print("got it")
         print(data.decode())
-        input()
-        s1.close()
-    if i == "2":
-        s1 = socket.socket()
-        s1.connect(SERVER_ADDRESS)
-        print("conected")
-        s1.send(b"01001Hey")
-        print("sent")
-        s1.close()
+    input()
+    s1.close()
 
 
 if __name__ == '__main__':
